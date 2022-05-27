5acd2db9add2b7b50ff7d79d0aacd2c781a12b22
ThisBodenmann <89189853+ThisBodenmann@users.noreply.github.com>
2022-05-27 11:25:09 +0200

    update

--- M226B_M120_Schulplaner/HomeworkWindow.xaml.cs
+++ M226B_M120_Schulplaner/HomeworkWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -28,6 +29,49 @@ namespace M226B_M120_Schulplaner
             SubjectWindow subjectWindow = new SubjectWindow();
             subjectWindow.Show();
             this.Close();
+        }
+        List<HomeWork> homeWork = new List<HomeWork>();
+        public class HomeWork
+        {
+            string subject;
+            string task;
+            DateTime date;
+            bool done;
+            public string Subject
+            {
+                get { return subject; }
+                set { subject = value; }
+            }
+            public string Task
+            {
+                get { return task; }
+                set { task = value; }
+            }
+            public DateTime Date
+            {
+                get { return date; }
+                set { date = value; }
+            }
+            public bool Done
+            {
+                get { return done; }
+                set { done = value; }
+            }
+        }
+        public ObservableCollection<HomeWork> HomeWorkList
+        {
+            get { return HomeWorkList;}
+            set { HomeWorkList = value; }
+        }
+
+        private void HomeWorkAdd_Click(object sender, RoutedEventArgs e)
+        {
+            HomeWork work = new HomeWork();
+            work.Subject = HomeWorkComboBox.SelectedValue.ToString()!;
+            work.Task = HomeWorkTaskBox.Text;
+            work.Date = Convert.ToDateTime(HomeWorkDateBox.Text);
+            work.Done = false;
+            homeWork.Add(work);
         }
     }
 }
